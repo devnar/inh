0e6744eb1b7a057b2b3499ce624c4e5fc3dde405
NAR <88266823+devnar@users.noreply.github.com>
2024-06-12 16:55:46 +0300

    Update math.js

--- math.js
+++ math.js
@@ -1,3 +1,15 @@
-topla: function (...args) {
-    return args.join(" ");
-},
+function add(bir,iki) {
+    echo(Number(bir) + Number(iki))
+}
+
+function subtract(bir,iki) {
+    echo(Number(bir) - Number(iki))
+}
+
+function divide(bir,iki) {
+    echo(Number(bir) + Number(iki))
+}
+
+function multiply(bir,iki) {
+    echo(Number(bir) + Number(iki))
+}
