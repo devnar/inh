480aa37445a80f5b442ac8bf885b119983e86e1d
NAR <yyazok@gmail.com>
2025-01-29 08:54:57 +0300

    3.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function help(script) {
     if(!script) {
-        echo("I'm Not Hacker [V 3.0] (c) NAR.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br><br>CLS &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Clears the screen.<br>COLOR &nbsp;&nbsp;&nbsp;&nbsp;Sets the default console foreground and background colors.<br>DATE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays or sets the date.<br>ECHO &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays messages, or turns command echoing on or off.<br>GOTO &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Directs the Windows command interpreter to a labeled line in a Web Site.<br>INHGET&nbsp;&nbsp;&nbsp;&nbsp;Upload and run codes from github or anywhere.<br>RELOAD&nbsp;&nbsp;&nbsp;&nbsp;Restarts the console.<br>TIME &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays or sets the system time.<br><br>For more information on tools see the command-line reference in the online help.");
+        echo("I'm Not Hacker [V 3.1] (c) NAR.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br><br>CLS &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Clears the screen.<br>COLOR &nbsp;&nbsp;&nbsp;&nbsp;Sets the default console foreground and background colors.<br>DATE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays or sets the date.<br>ECHO &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays messages, or turns command echoing on or off.<br>GOTO &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Directs the Windows command interpreter to a labeled line in a Web Site.<br>INHGET&nbsp;&nbsp;&nbsp;&nbsp;Upload and run codes from github or anywhere.<br>RELOAD&nbsp;&nbsp;&nbsp;&nbsp;Restarts the console.<br>TIME &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays or sets the system time.<br><br>For more information on tools see the command-line reference in the online help.");
     } else {
         eval(`${script}Help()`)
     }
@@ -33,55 +33,29 @@ function color(a) {
 }
 
 function inhget(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var script = document.createElement('script');
-            script.innerHTML = xhr.responseText;
-            document.body.appendChild(script);
+    if (url == "list") {
+        echo(JSON.parse(localStorage.getItem("jsFiles")) || []);
+    } else {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var script = document.createElement('script');
+                script.innerHTML = xhr.responseText;
+                document.body.appendChild(script);
+            }
+        };
+        const match = url.match(/[^/]+\.js$/);
+        if (match) {
+            const fileName = match[0];
+            let storedFiles = JSON.parse(localStorage.getItem("jsFiles")) || [];
+            if (!storedFiles.includes(fileName)) {
+                storedFiles.push(fileName);
+                localStorage.setItem("jsFiles", JSON.stringify(storedFiles));
+            }
+        } else {
+            echo("Uygun dosya adı bulunamadı.");
         }
-    };
-    xhr.send();
-}
-
-function dm() {
-    dmUser.forEach((u) => {
-        let rawUrl = localStorage.getItem("userRawUrl");
-        const username = rawUrl.split("/")[3];
-        const userFingerprint = localStorage.getItem("userFingerprint") + ".js";
-
-        rawUrl = rawUrl.replace(rawUrl.split("/")[3], u);
-        rawUrl = rawUrl.replace(userFingerprint, "dm.csv");
-
-        fetch(rawUrl)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`Error fetching data for ${u} ${response.statusText}`);
-                }
-                return response.text();
-            })
-            .then((data) => {
-                const lines = data.split('\n');
-                const userMessages = [];
-
-                lines.forEach((line) => {
-                    const [user, message] = line.split(',');
-                    if (user === username || user === "all") {
-                        userMessages.push(message.trim());
-                    }
-                });
-
-                if (userMessages.length > 0) {
-                    userMessages.forEach((msg) => {
-                        echo(`${u}: ${msg}`);
-                    });
-                } else {
-                    echo(`No messages found for ${username}`);
-                }
-            })
-            .catch((error) => {
-                echo(error);
-            });
-    });
+        xhr.send();
+    }
 }
