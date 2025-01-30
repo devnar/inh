af8646c16cfb9f0c9e69081772cd23191486ad4b
NAR <yyazok@gmail.com>
2025-01-30 15:11:05 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function help(script) {
     if(!script) {
-        echo("I'm Not Hacker [V 3.1] (c) NAR.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br><br>CLS &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Clears the screen.<br>COLOR &nbsp;&nbsp;&nbsp;&nbsp;Sets the default console foreground and background colors.<br>DATE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays or sets the date.<br>ECHO &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays messages, or turns command echoing on or off.<br>GOTO &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Directs the Windows command interpreter to a labeled line in a Web Site.<br>INHGET&nbsp;&nbsp;&nbsp;&nbsp;Upload and run codes from github or anywhere.<br>RELOAD&nbsp;&nbsp;&nbsp;&nbsp;Restarts the console.<br>TIME &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays or sets the system time.<br><br>For more information on tools see the command-line reference in the online help.");
+        echo("I'm Not Hacker [V 3.1] (c) NAR.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br><br>CLS &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Clears the screen.<br>COLOR &nbsp;&nbsp;&nbsp;&nbsp;Sets the default console foreground and background colors.<br>DATE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays or sets the date.<br>ECHO &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays messages, or turns command echoing on or off.<br>INHGET&nbsp;&nbsp;&nbsp;&nbsp;Upload and run codes from github or anywhere.<br>RELOAD&nbsp;&nbsp;&nbsp;&nbsp;Restarts the console.<br>TIME &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Displays or sets the system time.<br><br>For more information on tools see the command-line reference in the online help.");
     } else {
         eval(`${script}Help()`)
     }
